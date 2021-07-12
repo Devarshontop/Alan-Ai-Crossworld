3aa65b27361c14b45ce151fb9795af113496fc87
ssharsh <67879450+ssharsh@users.noreply.github.com>
2021-07-12 12:38:37 -0700

    h

--- assets/sketch.js
+++ assets/sketch.js
@@ -1,3 +1,12 @@
-function setup() {
-
-}
+var counter=0;
+	var timeleft=60;
+	function setup() {
+	noCanvas ();
+	}
+	var timer= select('#timer');
+	timer.html(counter);
+	function timeIT() {
+	counter++;
+	timer.html(counter);
+	}
+	setInterval(timeIt, 1000);
